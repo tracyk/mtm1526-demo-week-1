d2316d1902cd144b82b730726546926d620cd90b
tracyk <tracykarnabi@hotmail.com>
2012-01-13 11:35:33 -0500

    added information

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,13 @@
 // JavaScript Document
 /*Javascript multi -line comment
 */
+
+alert('Ohayo gozaimasu, hashimemashite Torechi desu!.'); //single or double quotes don't change anything
+alert ('I like unicorns'); // 'alert ()' pops up a little window with an ok button
+
+var name = prompt('What is your name beautiful?');
+console.log(name); // in javascript console.log is like trace in actionscript
+//console.log writes  out the content of a variablee for debugging 
+
+
+document.write('Hello, ' + name);
